da80f3a2dcbea16955b9bdfc53d188ac6ff82558
David H.O <cuentaporsi2@gmail.com>
2024-08-07 19:41:30 -0500

    working login

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+function showForm(formType) {
+    document.getElementById('registerForm').style.display = 'none';
+    document.getElementById('loginForm').style.display = 'none';
+
+    if (formType === 'register') {
+      document.getElementById('registerForm').style.display = 'block';
+    } else if (formType === 'login') {
+      document.getElementById('loginForm').style.display = 'block';
+    }
+  }
+  
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
